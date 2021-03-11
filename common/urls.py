04c2059b371a054fd9e1363cb4cfa9f7a39f91a5
DESKTOP-U224TA3\chjun <chjung@euclidsoft.co.kr>
2021-03-11 18:16:19 +0900

    파일 입력하면 저장가능한 객체 확인 -> toast로 UI에 띄워주기

--- common/urls.py
+++ common/urls.py
@@ -5,6 +5,6 @@ app_name = 'common'
 app_category_name = '공통'
 
 urlpatterns = [
-
-    url(r'^ajax/ndjson_file/save_in_elastic$', save_the_data_in_elastic, name='save_the_data_in_elastic'),
+    url(r'^ajax/detect_list_in_json$', detect_list_in_json, name='detect_list_in_json'),
+    url(r'^ajax/save_json_to_elasticsearch$', save_the_data_in_elastic, name='save_the_data_in_elastic'),
 ]
--- common/views.py
+++ common/views.py
@@ -11,6 +11,27 @@ from common.models import IndexingHistory
 
 
 @csrf_exempt
+def detect_list_in_json(request):
+    file = request.FILES.get('file')
+    try:
+        json_data = json.loads(file.read())
+        if str(type(json_data)) != "<class 'dict'>":
+            raise Exception
+    except Exception:
+        return JsonResponse({'result': False, 'msg': '잘못된 json 파일입니다.'})
+
+    lists = []
+    for k, v in json_data.items():
+        if str(type(v)) == "<class 'list'>":
+            lists.append(k)
+
+    if len(lists) == 0:
+        return JsonResponse({'result': False, 'msg': '저장시킬 수 있는 객체를 발견하지 못했습니다.'})
+    else:
+        return JsonResponse({'result': True, 'lists': lists, 'msg': '저장시킬 수 있는 객체를 발견하였습니다.'})
+
+
+@csrf_exempt
 def save_the_data_in_elastic(request):
     file = request.FILES.get('file')
     index_prefix = request.POST.get('index_prefix')
