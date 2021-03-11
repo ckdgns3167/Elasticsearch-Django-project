ad3182449f5fc821aa009314e6807eaf1f4b499f
DESKTOP-U224TA3\chjun <chjung@euclidsoft.co.kr>
2021-03-11 17:05:26 +0900

    json 파일 ndjson으로 바꿀 필요 없이 엘라스틱에 저장 가능.

--- common/models.py
+++ common/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class IndexingHistory(models.Model):
     index_name = models.CharField(max_length=200)
+    elastic_server_ip = models.CharField(max_length=20, default='127.0.0.1')
+    elastic_server_port = models.CharField(max_length=10, default='9200')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
