8b498d9f6ea0d2ec739c508f234beb0e9b7bf4de
DESKTOP-U224TA3\chjun <chjung@euclidsoft.co.kr>
2021-03-15 13:55:05 +0900

    인덱스 중복 검사 및 여러 예외 처리

--- common/urls.py
+++ common/urls.py
@@ -5,6 +5,8 @@ app_name = 'common'
 app_category_name = '공통'
 
 urlpatterns = [
+    url(r'^ajax/check_elasticsearch_connection$', check_elasticsearch_connection, name='check_elasticsearch_connection'),
     url(r'^ajax/detect_list_in_json$', detect_list_in_json, name='detect_list_in_json'),
     url(r'^ajax/save_json_to_elasticsearch$', save_the_data_in_elastic, name='save_the_data_in_elastic'),
+    url(r'^ajax/check_index_name_in_elastic$', check_index_name_in_elastic, name='check_index_name_in_elastic'),
 ]
--- common/views.py
+++ common/views.py
@@ -9,12 +9,34 @@ from elasticsearch import Elasticsearch, helpers
 
 from common.models import IndexingHistory
 
+DEFAULT_IP = '127.0.0.1';
+DEFAULT_PORT = '9200';
+
+
+@csrf_exempt
+def check_elasticsearch_connection(request):
+    es_ip = request.POST.get('es_ip')
+    es_port = request.POST.get('es_port')
+
+    if str(es_ip).strip() == '':
+        es_ip = DEFAULT_IP
+    if str(es_port).strip() == '':
+        es_port = DEFAULT_PORT
+
+    elastic = Elasticsearch(['http://' + es_ip + ':' + es_port], verify_certs=True)
+
+    if elastic.ping():
+        return JsonResponse({'result': True})
+    else:
+        return JsonResponse({'result': False})
+
 
 @csrf_exempt
 def detect_list_in_json(request):
     file = request.FILES.get('file')
     try:
         json_data = json.loads(file.read())
+        # 일단은 json이 최초 { 로 감싸진 것만 통과, 만약 [ 으로 감싸진 것은 제외시킴.
         if str(type(json_data)) != "<class 'dict'>":
             raise Exception
     except Exception:
@@ -69,3 +91,27 @@ def save_the_data_in_elastic(request):
         return JsonResponse({'result': True})
     else:
         return JsonResponse({'result': False, 'msg': '엘라스틱서치 연결 불가능'})
+
+
+@csrf_exempt
+def check_index_name_in_elastic(request):
+    es_ip = request.POST.get('es_ip')
+    es_port = request.POST.get('es_port')
+    index_prefix = request.POST.get('index_prefix')
+    objects_extracted_from_json = request.POST.getlist('objects_extracted_from_json[]')
+
+    if str(es_ip).strip() == '':
+        es_ip = DEFAULT_IP
+    if str(es_port).strip() == '':
+        es_port = DEFAULT_PORT
+
+    elastic = Elasticsearch(['http://' + es_ip + ':' + es_port], verify_certs=True)
+
+    if elastic.ping():
+        for item in objects_extracted_from_json:
+            if elastic.indices.exists(index=index_prefix + '-' + item):
+                return JsonResponse({'connection': True, 'already_exists': True})
+            else:
+                return JsonResponse({'connection': True, 'already_exists': False})
+    else:
+        return JsonResponse({'connection': False})
